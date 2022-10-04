7ca26970dc0415af75dc45c4c51519738d90c6a7
Artur Veske <artur.veske@coolbet.com>
2022-10-04 21:43:41 +0300

    assignment4

--- react-5-2/src/components/NewExpense/ExpenseForm.js
+++ react-5-2/src/components/NewExpense/ExpenseForm.js
@@ -103,6 +103,7 @@ const ExpenseForm = (props) => {
       </div>
       <div className="new-expense__actions">
         <button
+          type="button"
           onClick={() => {
             setIsFormOpened(false);
             clearFormInputs();
